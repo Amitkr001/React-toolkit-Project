577ca8e24f2aea48162b56c3d97b7478fc98dde0
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-21 04:03:29 +0530

    completed ui 90%

--- redux project/src/App.jsx
+++ redux project/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './components/nav'
 
 const App = () => {
   return (
-    <div>
-      
-      
-    </div>
+    <>
+    <Navbar/>
+    
+    </>
   )
 }
 
