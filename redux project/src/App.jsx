ac46a7d4afaa2a6e608a5d24766973f5bb88a6c3
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-21 03:47:47 +0530

    head section completed

--- redux project/src/App.jsx
+++ redux project/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>react app</h1>
-    </>
+    <div>
+      
+      
+    </div>
   )
 }
 
 export default App
-//
