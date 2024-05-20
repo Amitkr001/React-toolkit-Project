6421c208cb1a0da20ebca7f44906ab643ede942b
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-21 04:14:44 +0530

    user Details part done

--- redux project/src/App.jsx
+++ redux project/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/nav'
+import UserDetails from './components/UserDetails'
 
 const App = () => {
   return (
     <>
     <Navbar/>
-    
+    <UserDetails/>
     </>
   )
 }
